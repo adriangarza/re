09d4191f8cea1194c75dca6941e26ae24f027ac2
Adrian Garza <adrianhgarza@gmail.com>
2017-11-27 22:05:15 -0500

    better moving objects

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatform : MonoBehaviour {
+public class MovingObject : MonoBehaviour {
 
-	public GameObject platform;
+	public GameObject mainObject;
 	public GameObject pointA;
 	public GameObject pointB;
 	public float moveSpeed = 1;
@@ -15,18 +15,18 @@ public class MovingPlatform : MonoBehaviour {
 		if (pointA == null || pointB == null) {
 			Debug.LogError("You forgot to assign the movement endpoints!");
 		}
-		platform.transform.position = pointA.transform.position;
+		mainObject.transform.position = pointA.transform.position;
 	}
 
 	//moves from point A to point B and then back	
 	void Update () {
 		Move();
 		if (!returning) {
-			if (platform.transform.position == pointB.transform.position) {
+			if (mainObject.transform.position == pointB.transform.position) {
 				returning = true;
 			}
 		} else {
-			if (platform.transform.position == pointA.transform.position) {
+			if (mainObject.transform.position == pointA.transform.position) {
 				returning = false;
 			}
 		}
@@ -35,6 +35,6 @@ public class MovingPlatform : MonoBehaviour {
 	void Move() {
 		Vector3 target = returning ? pointA.transform.position : pointB.transform.position;
 		float step = moveSpeed * Time.deltaTime;
-        platform.transform.position = Vector3.MoveTowards(platform.transform.position, target, step);
+        mainObject.transform.position = Vector3.MoveTowards(mainObject.transform.position, target, step);
 	}
 }
