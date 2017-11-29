64334b30220d06276f37e7ddb7ab50704f6f16da
Adrian Garza <adrianhgarza@gmail.com>
2017-11-29 15:03:03 -0500

    jump pad animator

--- Assets/Scripts/JumpController.cs
+++ Assets/Scripts/JumpController.cs
@@ -7,9 +7,16 @@ public class JumpController : MonoBehaviour {
 	//launches the player in this direction on hit
 	public Vector2 launchVector;
 
+	Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator>();
+	}
+
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Player" || other.gameObject.tag == "corpse") {
 			other.gameObject.GetComponent<Rigidbody2D>().velocity = launchVector;
+			anim.SetTrigger("jump");
 		}
 	}
 }
